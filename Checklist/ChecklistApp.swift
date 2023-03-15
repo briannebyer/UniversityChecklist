f2fe4e1190b8468046a16aacedb89c00ed259784
briannebyer <briannemareebyer@mail.com>
2023-03-15 10:45:38 +1000

    Initial Commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Brianne Byer on 15/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Checklist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Checklist
+//
+//  Created by Brianne Byer on 15/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
